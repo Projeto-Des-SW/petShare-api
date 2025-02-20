6eac8fad04b5d7b50f19028578d87fbfa0b0bc92
Guilherme Felix <guilhermerutemfelix@gmail.com>
2025-02-20 11:40:12 -0300

    first commit

---
+++ petshare-backend/src/main/java/br/com/ufape/petshare/PetshareApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufape.petshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshareApplication.class, args);
+	}
+
+}
