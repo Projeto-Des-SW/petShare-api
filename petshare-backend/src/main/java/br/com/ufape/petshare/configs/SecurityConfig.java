fd9de0d596acbca26e56c0a8a9564dfa76a1f655
Guilherme Felix <guilhermerutemfelix@gmail.com>
2025-04-08 13:33:35 -0300

    Desativando autenticacao

--- petshare-backend/src/main/java/br/com/ufape/petshare/configs/SecurityConfig.java
+++ petshare-backend/src/main/java/br/com/ufape/petshare/configs/SecurityConfig.java
@@ -19,8 +19,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/public/**").permitAll() // libere o que precisar
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
             )
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter()))
